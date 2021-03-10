e82942467ee50ca8ccb6e7a0278e40e9b5f80e6a
Vikram singh rao <61112780+geekyviks@users.noreply.github.com>
2021-03-10 13:36:35 +0530

    Delete ndrp_drugs directory

--- ndrp_drugs/config.lua
+++
@@ -1,6 +0,0 @@
-Config = {}
-
-Config.CircleZones = {
-	WeedField = {coords = vector3(2037.89, 4909.04, 41.72), name = "Weed Field", color = 25, sprite = 496, radius = 100.0},
-	WeedProcessing = {coords = vector3(1394.36, 3601.83, 38.94), name = "Weed Processing", color = 25, sprite = 496, radius = 100.0},
-}
