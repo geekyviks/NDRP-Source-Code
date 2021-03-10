5d424342865c29a7b67d490620f75ab4af780957
Vikram singh rao <61112780+geekyviks@users.noreply.github.com>
2021-03-10 15:39:17 +0530

    Add files via upload

---
+++ [ESX]/skinchanger/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Skin Changer'
+
+version '1.0.3'
+
+client_scripts {
+	'locale.lua',
+	'locales/br.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/pl.lua',
+	'locales/sv.lua',
+	'config.lua',
+	'client/main.lua'
+}
